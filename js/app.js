67a356963413978b3941985a2e262be46cd5d205
cganzon <crganzon@gmail.com>
2022-09-12 20:08:03 -0700

    Log the username input value to the console when the search button is clicked

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 const usernameInput = document.querySelector(".username");
 const searchBtn = document.querySelector(".search-btn");
 
-searchBtn.addEventListener("click", () => {
-    alert("Clicked")
-})
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const usernameValue = usernameInput.value.trim();
+    console.log(usernameValue);
+});
 
 fetch("https://api.github.com/users/octocat")
     .then(res => res.json())

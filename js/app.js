cc2bd4d9600a1edaea25cade09fabaa27bb273bd
cganzon <crganzon@gmail.com>
2022-09-12 20:14:34 -0700

    Add error handling for if the github username does not exist

--- js/app.js
+++ js/app.js
@@ -4,8 +4,14 @@ const searchBtn = document.querySelector(".search-btn");
 searchBtn.addEventListener("click", (e) => {
     e.preventDefault();
     const usernameValue = usernameInput.value.trim();
-    
+
     fetch(`https://api.github.com/users/${usernameValue}`)
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => {
+        if(data.message === "Not Found") {
+            console.log("No results");
+        } else {
+            console.log(data);
+        };
+    });
 });

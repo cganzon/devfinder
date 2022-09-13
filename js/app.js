8ffeb479983c0c08a23726c3e261bb608bb55733
cganzon <crganzon@gmail.com>
2022-09-12 20:05:20 -0700

    Add click listener to search button. Add defer attribut to the js script

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 const usernameInput = document.querySelector(".username");
 const searchBtn = document.querySelector(".search-btn");
 
+searchBtn.addEventListener("click", () => {
+    alert("Clicked")
+})
+
 fetch("https://api.github.com/users/octocat")
     .then(res => res.json())
     .then(data => console.log(data));

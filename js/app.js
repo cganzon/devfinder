471771d90232c039dc00660ecff911e14a601262
cganzon <crganzon@gmail.com>
2022-09-12 20:10:40 -0700

    Call the github api with the username input value when the search button is clicked

--- js/app.js
+++ js/app.js
@@ -4,9 +4,8 @@ const searchBtn = document.querySelector(".search-btn");
 searchBtn.addEventListener("click", (e) => {
     e.preventDefault();
     const usernameValue = usernameInput.value.trim();
-    console.log(usernameValue);
-});
-
-fetch("https://api.github.com/users/octocat")
+    
+    fetch(`https://api.github.com/users/${usernameValue}`)
     .then(res => res.json())
     .then(data => console.log(data));
+});

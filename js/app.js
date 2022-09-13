bd5da7e02500d6f13fe116a963c5fc47df175be4
cganzon <crganzon@gmail.com>
2022-09-12 20:03:20 -0700

    Add class names to form elements and save input and search button as variables in js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+const usernameInput = document.querySelector(".username");
+const seacrchBtn = document.querySelector(".search-btn");
+
 fetch("https://api.github.com/users/octocat")
     .then(res => res.json())
     .then(data => console.log(data));

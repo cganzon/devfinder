ea3091143b6a4767b98400c9ec918d3ff2a56e22
cganzon <crganzon@gmail.com>
2022-09-14 21:06:15 -0700

    Switch between the sun and moon icons when the theme toggle button is clicked

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,11 +1,13 @@
 let darkMode = localStorage.getItem("dark-mode");
 const themeToggleBtn = document.querySelector(".theme-toggle-button");
+const moonIcon = document.querySelector("#moon");
+const sunIcon = document.querySelector("#sun");
 
 if(darkMode === "enabled") {
     enableDarkMode();
 } else {
     disableDarkMode();
-}
+};
 
 themeToggleBtn.addEventListener("click", () => {
     darkMode = localStorage.getItem("dark-mode");
@@ -19,9 +21,13 @@ themeToggleBtn.addEventListener("click", () => {
 function enableDarkMode() {
     document.body.classList.add("dark-mode");
     localStorage.setItem("dark-mode", "enabled");
+    moonIcon.classList.remove("show");
+    sunIcon.classList.add("show");
 };
 
 function disableDarkMode() {
     document.body.classList.remove("dark-mode");
     localStorage.setItem("dark-mode", null);
+    moonIcon.classList.add("show");
+    sunIcon.classList.remove("show");
 };
